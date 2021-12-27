fce22c136cc382a498678c9e8a70805d3d3912e5
ang8210 <m22715286@gmail.com>
2021-12-27 09:19:10 +0800

    modified main program & add logfile

--- app.py
+++ app.py
@@ -1,97 +1,138 @@
 from flask import Flask, request
 import sqlite3 as sql
 import json
+import logging
+import logging.config
+logging.config.fileConfig('./logfile/logging.conf')
+logger = logging.getLogger()
 
 app = Flask(__name__)
 
 database = "shopDatabase.db"
 
-
-def getData():
-    with sql.connect(database) as con:
-        cur = con.cursor()
-        cur.execute("select * from PRODUCT")
-        rows = cur.fetchall()
-        return rows
-
-
 @app.route('/list', methods=['GET'])
 def list():
-    db = getData()
-    print(db)
-    arr = []
-    for i in range(len(db)):
-        arr.append({
-            "id" : db[i][0],
-            "item" : db[i][1],
-            "price" : db[i][2],
-            "quantity" : db[i][3]
-            })
-
-    return json.dumps(arr)
+    try:
+        with sql.connect(database) as con:
+            cur = con.cursor()
+            cur.execute("select * from PRODUCT")
+            rows = cur.fetchall()
+            logger.debug(rows)
+            arr = []
+            for i in range(len(rows)):
+                arr.append({
+                    "id" : rows[i][0],
+                    "item" : rows[i][1],
+                    "price" : rows[i][2],
+                    "quantity" : rows[i][3]
+                    })
+            statusCode = 200
+            msg = "Record successfully added"
+    except:
+        con.rollback()
+        statusCode = 500
+        msg = "error in insert operation"
+    finally:
+        con.close()
+        results = {
+            "statusCode" : statusCode,
+            "msg" : msg,
+            "list" : arr
+        }
+        return json.dumps(results)
 
 
 @app.route('/addItem', methods=['POST'])
 def addItem():
     try:
-        item = request.form['item']
-        price = request.form['price']
-        quantity = request.form['quantity']
+        # form-data
+        # item = request.form['item']
+        # price = request.form['price']
+        # quantity = request.form['quantity']
+
+        item = request.json['item']
+        price = request.json['price']
+        quantity = request.json['quantity']
 
         with sql.connect(database) as con:
             cur = con.cursor()
-            cur.execute("INSERT INTO PRODUCT(item,price,quantity) VALUES (?,?,?)", (item, price, quantity))
-            con.commit()
-            msg = "Record successfully added"
+            cur.execute("SELECT * FROM PRODUCT WHERE item = ? ;", (item,))
+            row = cur.fetchall()
+            logger.debug(row)
+            if row == None:
+                cur.execute("INSERT INTO PRODUCT(item,price,quantity) VALUES (?,?,?)", (item, price, quantity))
+                con.commit()
+                statusCode = 200
+                msg = "Record successfully added"
+            else:
+                statusCode = 400
+                msg = "Record repeated addition"
     except:
         con.rollback()
+        statusCode = 500
         msg = "error in insert operation"
 
     finally:
         con.close()
-        return msg
+        results = {
+            "statusCode" : statusCode,
+            "msg" : msg,
+        }
+        return json.dumps(results)
 
 
-@app.route('/deleteItem/<int:id>', methods=['GET'])
-def deleteItem(id):
-    id = int(id)
+@app.route('/deleteItem', methods=['GET'])
+def deleteItem():
+    id = int(request.args['id'])
     try:
         with sql.connect(database) as con:
             cur = con.cursor()
-            cur.execute("DELETE FROM PRODUCT WHERE id=?", (id))
+            cur.execute("DELETE FROM PRODUCT WHERE id=?", (id,))
             con.commit()
+            statusCode = 200
             msg = "Record successfully deleted"
     except:
         con.rollback()
+        statusCode = 500
         msg = "error in delete operation"
     finally:
         con.close()
-        return msg
+        results = {
+            "statusCode" : statusCode,
+            "msg" : msg,
+        }
+        return json.dumps(results)
 
 
-@app.route('/modifyPrice/<int:id>/<int:pic>', methods=['GET'])
-def modifyPrice(id, pic):
-    id = int(id)
-    price = int(pic)
+@app.route('/modifyPrice', methods=['GET'])
+def modifyPrice():
+    id = int(request.args['id'])
+    price = int(request.args['price'])
     try:
         with sql.connect(database) as con:
             cur = con.cursor()
-            cur.execute("UPDATE PRODUCT SET price=? WHERE id=?;", (pic, id))
+            cur.execute("UPDATE PRODUCT SET price=? WHERE id=?;", (price, id))
             con.commit()
+            statusCode = 200
             msg = "Record change successfully"
     except:
         con.rollback()
+        statusCode = 500
         msg = "error in change operation"
     finally:
         con.close()
-        return msg
+        results = {
+            "statusCode" : statusCode,
+            "msg" : msg,
+        }
+        return json.dumps(results)
 
 
 @app.route('/order', methods=['POST'])
 def order():
     try:
         order = request.json["order"]
-        print(order)
+        logger.debug(order)
 
         total = 0
         arr = []
@@ -114,32 +155,54 @@ def order():
                 remaingQuantity = row[0][3] - orderQuantity
                 cur.execute("UPDATE PRODUCT SET quantity = ? WHERE id = ?;",(remaingQuantity,arr[i]['id'],))
                 con.commit()
+            statusCode = 200    
             msg = "Record successfully order"
-        return {"msg": msg ,"order":arr,"total":total}
     except:
         con.rollback()
-        return "error in order operation"
+        statusCode = 500
+        msg = "error in order operation"
 
     finally:
         con.close()
+        results = {
+            "statusCode" : statusCode,
+            "msg" : msg,
+            "order":arr,
+            "total":total
+        }
+        return json.dumps(results)
 
 
-@app.route('/select/<string:item>', methods=['GET'])
-def select(item):
-    with sql.connect(database) as con:
-        # item = "\'"+item+"\'"
-        print(item)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM PRODUCT WHERE item = ? ;", (item,))
-        row = cur.fetchall()
-        print(row)
-        arr = []
-        for i in range(len(row)):
-            arr.append({
-                "id" : row[i][0],
-                "item" : row[i][1],
-                "price" : row[i][2],
-                "quantity" : row[i][3]
-                })
-
-        return json.dumps(arr)
+@app.route('/select', methods=['GET'])
+def select():
+    try:
+        item = request.args['item']
+        with sql.connect(database) as con:
+            # item = "\'"+item+"\'"
+            logger.debug(item)
+            cur = con.cursor()
+            cur.execute("SELECT * FROM PRODUCT WHERE item = ? ;", (item,))
+            row = cur.fetchall()
+            logger.debug(row)
+            arr = []
+            for i in range(len(row)):
+                arr.append({
+                    "id" : row[i][0],
+                    "item" : row[i][1],
+                    "price" : row[i][2],
+                    "quantity" : row[i][3]
+                    })
+            statusCode = 200    
+            msg = "Select successfully order"
+    except:
+        con.rollback()
+        statusCode = 500
+        msg = "error in select operation"
+    finally:    
+        con.close()
+        results = {
+            "statusCode" : statusCode,
+            "msg" : msg,
+            "select":arr
+        }
+        return json.dumps(results)
